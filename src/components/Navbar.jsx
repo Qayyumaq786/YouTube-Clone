e60f09ba8010de66ef03cf71e316f3c4d023ebae
Qayyumaq786 <54434814+Qayyumaq786@users.noreply.github.com>
2023-05-20 16:39:35 +0530

    fd

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import { Stack } from "@mui/material";
+import { Stack, colors } from "@mui/material";
 import { Link } from "react-router-dom";
 
 import { logo } from "../utils/constants";
@@ -7,7 +7,7 @@ import { SearchBar } from "./";
 const Navbar = () => (
   <Stack direction="row" alignItems="center" p={2} sx={{ position:  "sticky", background: '#000', top: 0, justifyContent: "space-between" }}>
     <Link to="/" style={{ display: "flex", alignItems: "center" }}>
-      <img  src={logo} alt="logo" height={45} />
+      <img  src={logo} alt="logo" height={45} /><h3 style={{color:'white' ,fontWeight:'bold'}}>YouTube</h3>
     </Link>
     <SearchBar />
   </Stack>
