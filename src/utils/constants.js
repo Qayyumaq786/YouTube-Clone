10289d2301b7e032dbdbd168b4ea0e1270cd9d6f
Qayyumaq786 <54434814+Qayyumaq786@users.noreply.github.com>
2023-05-20 16:26:28 +0530

    first commit

---
+++ src/utils/constants.js
@@ -0,0 +1,41 @@
+import MusicNoteIcon from '@mui/icons-material/MusicNote';
+import HomeIcon from '@mui/icons-material/Home';
+import CodeIcon from '@mui/icons-material/Code';
+import OndemandVideoIcon from '@mui/icons-material/OndemandVideo';
+import SportsEsportsIcon from '@mui/icons-material/SportsEsports';
+import LiveTvIcon from '@mui/icons-material/LiveTv';
+import SchoolIcon from '@mui/icons-material/School';
+import FaceRetouchingNaturalIcon from '@mui/icons-material/FaceRetouchingNatural';
+import CheckroomIcon from '@mui/icons-material/Checkroom';
+import GraphicEqIcon from '@mui/icons-material/GraphicEq';
+import TheaterComedyIcon from '@mui/icons-material/TheaterComedy';
+import FitnessCenterIcon from '@mui/icons-material/FitnessCenter';
+import DeveloperModeIcon from '@mui/icons-material/DeveloperMode';
+import TrendingUpIcon from '@mui/icons-material/TrendingUp';
+
+
+export const logo = 'https://i.pinimg.com/564x/a3/45/a1/a345a1abc97abc337d7cb967436a3a18.jpg';
+
+export const categories = [
+  { name: 'New', icon: <HomeIcon />, },
+  { name: 'T-Series', icon: <MusicNoteIcon />, },
+  { name: 'Trendings', icon: <TrendingUpIcon />, },
+  { name: 'Live', icon: <LiveTvIcon />, },
+  { name: 'Music', icon: <MusicNoteIcon /> },
+  { name: 'Movie', icon: <OndemandVideoIcon />, },
+  { name: 'Education', icon: <SchoolIcon />, },
+  { name: 'Gaming', icon: <SportsEsportsIcon />, },
+  { name: 'Sport', icon: <FitnessCenterIcon />, },
+  { name: 'Fashion', icon: <CheckroomIcon />, },
+  { name: 'Beauty', icon: <FaceRetouchingNaturalIcon />, },
+  { name: 'Comedy', icon: <TheaterComedyIcon />, },
+  { name: 'Gym', icon: <FitnessCenterIcon />, },
+  
+];
+
+export const demoThumbnailUrl = 'https://image.cnbcfm.com/api/v1/image/106528417-1588945694643gettyimages-1206290114.jpeg?v=1603313009';
+export const demoChannelUrl = '/channel/UCm-Gu4dipIz-fJB0QfUk3pw';
+export const demoVideoUrl = '/video/KH_aVdva1g0';
+export const demoChannelTitle = 'Code With Qayyum';
+export const demoVideoTitle = 'this website maded by qayyum using react.js and rapid api';
+export const demoProfilePicture = 'http://dergipark.org.tr/assets/app/images/buddy_sample.png'
